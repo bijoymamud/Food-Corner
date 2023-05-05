ee7d1b37b61f39f657ef67ba0d3116115a46ba33
bijoymamud <bijoymamud.09@gmail.com>
2023-05-05 15:50:50 +0600

    header modify

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -19,24 +19,22 @@ const Header = () => {
             <div className="navbar bg-base-200 md:px-20">
                 <div className="navbar-start  mx-auto">
                     <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <NavLink to="/" className={(isActive) => (isActive ? "underline" : "")}>Home</NavLink>
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                                <Link to="/"><a>Home</a></Link>
+                                <Link to="/about" tabIndex={0}>
+
+                                    About
 
 
-                        </ul>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <NavLink to="/about" className={(isActive) => (isActive ? "underline" : "")}>About</NavLink>
 
-
-                        </ul>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <NavLink to="/blog" className={(isActive) => (isActive ? "underline" : "")}>Blog</NavLink>
-
-
-                        </ul>
+                                </Link>
+                                <Link to="/blog">Blog</Link>
+                            </ul>
+                        </div>
                     </div>
                     <a className="btn btn-ghost normal-case md:text-3xl font-extrabold font-mono">Food Corner</a>
                 </div>
